c93909810eae30e847859f5e2e7c4cf18271d97b
silva-everton <amorimsilva.everton@gmail.com>
2024-09-19 19:44:31 +1000

    login page design

--- src/app/screens/Login/index.js
+++ src/app/screens/Login/index.js
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
+import Colors from '../../../constants/Colors';
 
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.loginText}>Design the Login Screen here!!</Text>
-      <Link href="/(tabs)/home">
+      <Text style={styles.loginText}>Design the Login Screen here!</Text>
+      <Link href="/(tabs)/home" style={styles.loginLink}>
         <Text style={styles.linkText}>Login</Text>
       </Link>
     </View>
@@ -26,8 +27,22 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   linkText: {
-    fontSize: 18,
-        
+    fontSize: 30,
+    fontFamily: 'NunitoSans-extraBold',
+    color: Colors.SOFTCREAM,
+    marginTop: 20,  
+    position: 'absolute',
+    bottom: 20,
     
+  },
+  loginLink: {
+    position: 'absolute',
+    bottom: 100, 
+    width: '80%',
+    borderColor: Colors.TURQUOISE,
+    borderRadius: 10, 
+    backgroundColor: Colors.TURQUOISE,
+    textAlign: 'center',    
+    alignItems: 'center',
   },
 });
