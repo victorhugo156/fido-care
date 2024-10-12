79a46b02af64708ddc4b1b5e6dc335225dd8d06e
Victor Hugo <victorhugo156@gmail.com>
2024-10-12 18:51:23 +1100

    Working on Filter and Sending data from Home to Filter screen

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -1,75 +1,81 @@
 import { Image, SafeAreaView, View, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { useState } from 'react';
 import { Tabs } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Entypo from '@expo/vector-icons/Entypo';
 import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+import { FilterServiceContext } from '../Context/filterServiceContext';
+import { FilterServiceContextProvider } from '../Context/filterServiceContext';
 import Colors from '../../constants/Colors';
 import Font_Family from "../../constants/Font_Family";
 import Font_Size from "../../constants/Font_Size";
 
 
-export default function TabLayout() {
+export default function TabLayout({ children }) {
+    // const [service, setService] = useState("Pet Sitter");
+
     return (
-        <Tabs screenOptions={{
-            tabBarActiveTintColor: Colors.BRIGHT_BLUE,
-            tabBarInactiveTintColor: 'white',
-            
-            tabBarStyle: {
-                
-                backgroundColor: Colors.TURQUOISE_GREEN,
-                borderTopColor: 'transparent',
-                elevation: 0,
-                height: 70
-            },
+            <Tabs screenOptions={{
+                tabBarActiveTintColor: Colors.BRIGHT_BLUE,
+                tabBarInactiveTintColor: 'white',
 
-            tabBarLabelStyle:{
-                fontSize: Font_Size.LG,
-                fontFamily: Font_Family.REGULAR,
-            },
+                tabBarStyle: {
 
-            headerShown: true,
-        }}
-        >
-            <Tabs.Screen name='Home'
-                options={{
-                    path: 'Home/index',
-                    tabBarLabel: "Home",
-                    headerShown: false,
+                    backgroundColor: Colors.TURQUOISE_GREEN,
+                    borderTopColor: 'transparent',
+                    elevation: 0,
+                    height: 70
+                },
 
-                    tabBarIcon: ({ color }) => <FontAwesome name="home" size={32} color={color} />,
-                }}
+                tabBarLabelStyle: {
+                    fontSize: Font_Size.LG,
+                    fontFamily: Font_Family.REGULAR,
+                },
 
-            />
+                headerShown: true,
+            }}
+            >
+                <Tabs.Screen name='Home'
+                    options={{
+                        path: 'Home/index',
+                        tabBarLabel: "Home",
+                        headerShown: false,
 
-            <Tabs.Screen name="Bookings/index"
-                options={{
-                    path: 'Bookings',
-                    tabBarLabel: "Bookings",
-                    tabBarIcon: ({ color }) => <Entypo name="calendar" size={32} color={color} />
-                }}
-            />
-            <Tabs.Screen name="Chat/index"
-                options={{
-                    path: 'Chat',
-                    tabBarLabel: "Chat",
-                    tabBarIcon: ({ color }) => <Ionicons name="chatbox" size={32} color={color} />
-                }}
-            />
-            <Tabs.Screen name="Menu/index"
-                options={{
-                    path: 'Menu',
-                    tabBarLabel: "Menu",
-                    tabBarIcon: ({ color }) => <FontAwesome name="gear" size={32} color={color} />
-                }}
-            />
-        </Tabs>
+                        tabBarIcon: ({ color }) => <FontAwesome name="home" size={32} color={color} />,
+                    }}
 
+                />
+
+                <Tabs.Screen name="Bookings/index"
+                    options={{
+                        path: 'Bookings',
+                        tabBarLabel: "Bookings",
+                        tabBarIcon: ({ color }) => <Entypo name="calendar" size={32} color={color} />
+                    }}
+                />
+                <Tabs.Screen name="Chat/index"
+                    options={{
+                        path: 'Chat',
+                        tabBarLabel: "Chat",
+                        tabBarIcon: ({ color }) => <Ionicons name="chatbox" size={32} color={color} />
+                    }}
+                />
+                <Tabs.Screen name="Menu/index"
+                    options={{
+                        path: 'Menu',
+                        tabBarLabel: "Menu",
+                        tabBarIcon: ({ color }) => <FontAwesome name="gear" size={32} color={color} />
+                    }}
+                />
+            </Tabs>
     );
 }
 
 const styles = StyleSheet.create({
-    ContainerSafeArea:{
+    ContainerSafeArea: {
         paddingTop: 20,
 
     },
@@ -79,7 +85,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    Logo:{
+    Logo: {
         width: 100,
         resizeMode: "contain"
     },
@@ -87,16 +93,16 @@ const styles = StyleSheet.create({
         marginLeft: 20,
     },
 
-    Paw:{
+    Paw: {
         width: 50,
         resizeMode: "contain"
     },
 
-    HeaderRightContainer:{
+    HeaderRightContainer: {
         height: "80%",
 
         marginRight: 20,
 
-        justifyContent:"flex-end"
+        justifyContent: "flex-end"
     }
 });
--- src/app/screens/Filter/index.js
+++ src/app/screens/Filter/index.js
@@ -1,30 +1,76 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { View, Text, Image, TouchableOpacity, FlatList, StyleSheet, TextInput, Dimensions, SafeAreaView } from 'react-native';
-import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useRouter } from 'expo-router';
+import Slider from '@react-native-community/slider';
 
+import { useFilterServiceContext } from '../../hook/useFilterServiceContext';
 import ButtonApply from '../../../components/ButtonApply/idex';
 import Colors from '../../../constants/Colors';
 import Font_Family from '../../../constants/Font_Family';
 import Font_Size from '../../../constants/Font_Size';
 
-export default function FilterScreen(){
+export default function FilterScreen() {
+
+    const { service } = useFilterServiceContext();
+
+    const [price, setPrice] = useState(0)
+
+    // useEffect(() => {
+
+    //     console.log("Service in Filter Screen:", service); // Log whenever Filter Screen is focused
+    // }, [service]);
 
     const router = useRouter(); // Initialize router
 
-    const{ service } = useLocalSearchParams();
+    return (
 
-    return(
         <SafeAreaView style={styles.Container}>
             <View style={styles.ContainerCard}>
+                <View style={styles.ContainerTitle}>
+                    <View style={styles.ContainerMagnifyingGlass}>
+                        <Image style={styles.MagnifyingGlass} source={require("../../../assets/icons/magnifiying-glass.png")} />
+                    </View>
+                    <View style={styles.ContainerTxt}>
+                        <Text style={styles.TxtTitle}>Find you Pet Sitter</Text>
+                        <Text style={styles.TxtDescription}>Tell us a bit more about your pet and.{"\n"}
+                            we can help you to find the best Pet Sitter</Text>
+                    </View>
+
+                </View>
 
                 {/*Card Service */}
                 <TouchableOpacity style={styles.ContainerRow} onPress={() => router.push('screens/FilterService')}>
                     <Image style={styles.Icon} source={require("../../../assets/icons/suitcase.png")} />
                     <View style={styles.ContainerLabels}>
                         <Text style={styles.ServiceLabel}>Service</Text>
-                        <Text style={styles.ResultLabel}>{service ? service : "Select Filter"}</Text>
+                        <Text style={styles.ResultLabel}>{service}</Text>
                     </View>
                 </TouchableOpacity>
+
+                <View style={styles.ContainerSlider}>
+                    <Slider style={{ width: 200, height: 60 }}
+                        onValueChange={(value) => setPrice(value)}
+                        minimumValue={22}
+                        maximumValue={100}
+                        minimumTrackTintColor={Colors.BRIGHT_BLUE}
+                        maximumTrackTintColor={Colors.GRAY_600}
+                        thumbTintColor={Colors.TURQUOISE_GREEN} // Color for thumb (control)
+                        thumbStyle={{ width: 80, height: 80 }}  // Control thumb size
+                        trackStyle={{ height: 30 }} />
+
+                    <View style={styles.ContainerPrices}>
+                        <View style={styles.ContainerMinPrice}>
+                            <Text style={styles.TxtDescription}>AU$ 22</Text>
+                        </View>
+
+                        <Text style={styles.TxtDescription}>up to</Text>
+
+                        <View style={styles.ContainerMaxPrice}>
+                            <Text style={styles.TxtDescription}>AU$ {Math.floor(price)}</Text>
+                        </View>
+                    </View>
+                </View>
 
                 {/*Card Location */}
                 <TouchableOpacity style={styles.ContainerRow}>
@@ -49,31 +95,121 @@ export default function FilterScreen(){
                 <ButtonApply bgColor={Colors.CORAL_PINK} btnTitle={"Apply"} />
                 <ButtonApply bgColor={Colors.GRAY_200} btnTitle={"Clean"} />
             </View>
-           
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
 
-    Container:{
+    Container: {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        gap: 180
+        gap: 100
     },
 
-    ContainerCard:{
+    ContainerCard: {
+        width: "100%",
 
     },
 
-    ContainerBtn:{
+    MagnifyingGlass: {
+        tintColor: Colors.WHITE,
+
+        width: 30,
+        height: 30,
+
+    },
+
+    ContainerTitle: {
+        width: "100%",
+
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 20,
+
+        paddingHorizontal: 15,
+
+        marginBottom: 20
+    },
+
+    TxtTitle: {
+        fontFamily: Font_Family.BLACK,
+        fontSize: Font_Size.XL,
+        color: Colors.GRAY_700,
+
+        paddingVertical: 5,
+    },
+    TxtDescription: {
+        fontFamily: Font_Family.REGULAR,
+        fontSize: Font_Size.MD,
+        color: Colors.GRAY_700,
+
+    },
+
+    ContainerMagnifyingGlass: {
+        backgroundColor: Colors.BRIGHT_BLUE,
+
+        width: 55,
+        height: 55,
+
+        alignItems: "center",
+        justifyContent: "center",
+
+        borderRadius: 55
+    },
+
+    ContainerSlider: {
+        width: "100%",
+        height: 120,
+
+        alignItems: "center",
+
+        marginBottom: 30
+
+    },
+
+    ContainerPrices:{
+        width: "100%",
+
+        flexDirection: "row",
+
+        justifyContent: "space-between",
+        alignItems: "center",
+
+        paddingHorizontal: 30
+    },
+
+    ContainerMinPrice:{
+        width: 120,
+        height: 40,
+
+        justifyContent: "center",
+        alignItems: "center",
+
+        borderWidth: 2,
+        borderColor: Colors.GRAY_600,
+    },
+
+    ContainerMaxPrice:{
+        width: 120,
+        height: 40,
+
+        justifyContent: "center",
+        alignItems: "center",
+
+        borderWidth: 2,
+        borderColor: Colors.GRAY_600,
+
+    },
+
+    ContainerBtn: {
         flexDirection: "row",
 
         gap: 50,
     },
 
-    ContainerRow:{
+    ContainerRow: {
         backgroundColor: Colors.GRAY_100,
         width: "100%",
         height: 100,
@@ -85,9 +221,11 @@ const styles = StyleSheet.create({
 
         borderBottomWidth: 2,
         borderBottomColor: Colors.GRAY_200,
+
+        marginBottom: 10
     },
 
-    Icon:{
+    Icon: {
         width: 32,
         height: 32,
 
@@ -97,8 +235,7 @@ const styles = StyleSheet.create({
 
     },
 
-    ContainerLabels:{
-
+    ContainerLabels: {
         width: 300,
 
         flexDirection: "row",
@@ -106,13 +243,13 @@ const styles = StyleSheet.create({
 
     },
 
-    ServiceLabel:{
+    ServiceLabel: {
         fontFamily: Font_Family.REGULAR,
         fontSize: Font_Size.MD,
         color: Colors.GRAY_700,
     },
 
-    ResultLabel:{
+    ResultLabel: {
         fontFamily: Font_Family.BOLD,
         fontSize: Font_Size.MD,
         color: Colors.GRAY_700,
