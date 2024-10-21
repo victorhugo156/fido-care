773d138a1cd064654e7f92b828b6a848f5c0c199
silva-everton <amorimsilva.everton@gmail.com>
2024-10-21 14:36:44 +1100

    _layout.js updated

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -264,6 +264,8 @@ export default function Rootlayout() {
                         }}
                     />
 
+                    
+
                 </Stack>
             </SafeAreaView>
         </FilterServiceContextProvider>
